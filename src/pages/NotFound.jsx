d3be3f934a7159a961b7f06ade4a923b30c96c8a
Christo <kestreljack7@gmail.com>
2026-01-22 21:17:48 +0530

    Notfound added

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,26 @@
+import { Link, useLocation } from "react-router-dom";
+
+function NotFound() {
+  const location = useLocation();
+
+  return (
+    <div
+      style={{
+        padding: "40px",
+        textAlign: "center",
+      }}
+    >
+      <h1>404 - Page Not Found</h1>
+
+      <p>
+        The page <strong>{location.pathname}</strong> does not exist.
+      </p>
+
+      <br />
+
+      <Link to="/">Go back to Home</Link>
+    </div>
+  );
+}
+
+export default NotFound;
